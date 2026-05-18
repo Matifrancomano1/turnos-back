44d1913af7ed09d6df05ab224d753aaaa598fd3c
matifrancomano1 <francomanomatias@gmail.com>
2026-05-18 11:03:26 -0300

    Migracion bd

--- target/generated-sources/annotations/com/turnos/saas/mapper/EmpresaMapperImpl.java
+++ target/generated-sources/annotations/com/turnos/saas/mapper/EmpresaMapperImpl.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-13T17:36:27-0300",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.0.v20260407-0427, environment: Java 21.0.10 (Eclipse Adoptium)"
+    date = "2026-05-18T10:42:45-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
 public class EmpresaMapperImpl implements EmpresaMapper {
